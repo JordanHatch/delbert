6f0664ce47da6901e8d182d3831ab13ce423f448
Jordan Hatch <jordan@jordanh.net>
2012-10-01 10:49:47 +0100

    Add refresh timer

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -22,11 +22,34 @@ var render_pull_requests = function(data) {
 }
 
 var update_pull_requests = function() {
+  var refresh_delay = 30e3;
   $.getJSON('/requests.json', function(data){
     render_pull_requests(data);
   });
-  setTimeout(update_pull_requests, 60 * 1000)
+  timer(refresh_delay);
+  setTimeout(update_pull_requests, refresh_delay);
 }
+
+// from https://github.com/edds/jenkins-dashboard/blob/master/javascripts/main.js
+var timer = function(duration){
+  var loader = document.getElementById("refresh-timer"),
+      end = +new Date() + duration,
+      radius = 12,
+      interval;
+
+  interval = window.setInterval(function(){
+    if(+new Date() > end){
+      window.clearInterval(interval);
+      return true;
+    }
+    var degrees = 360 - 360*((end - new Date())/duration),
+        y = -Math.cos((degrees/180)*Math.PI)*radius,
+        x = Math.sin((degrees/180)*Math.PI)*radius,
+        mid = (degrees < 180) ? '0,1' : '1,1',
+        d = "M"+radius+","+radius+" v -"+radius+" A"+radius+","+radius+" 1 "+mid+" " + (x+radius) + "," + (y+radius) + " z";
+    loader.setAttribute("d", d);
+  }, 50);
+};
 
 $(function() {
   update_pull_requests();
